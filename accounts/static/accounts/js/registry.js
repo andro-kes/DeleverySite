7561b7da979f8a04af0a42d80525b547e57ac861
Mikhail <67060204+flowtop@users.noreply.github.com>
2024-04-09 16:28:20 +0300

    Create registry.js

---
+++ accounts/static/accounts/js/registry.js
@@ -0,0 +1,20 @@
+let tabsBtn = document.querySelectorAll(".registry__switch .switch__btn");
+
+tabsBtn.forEach(i => {
+    i.onclick = () => {
+        document.querySelector(".registry__tab.tab_active").classList.remove("tab_active");
+        let tabClass = i.dataset.tab;
+        document.querySelector(".registry__tab." + tabClass).classList.add("tab_active");
+
+        document.querySelector(".registry__switch .switch__btn._active").classList.remove("_active");
+        i.classList.add("_active");
+    };
+});
+
+let checkboxes = document.querySelectorAll(".checkbox__button");
+
+checkboxes.forEach(i => {
+    i.onclick = () => {
+        i.classList.toggle("_checked");
+    };
+});
