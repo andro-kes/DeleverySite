f7b64ad8765f4a0e78d370e287eeb45a45cc6128
Mikhail <67060204+flowtop@users.noreply.github.com>
2024-04-10 17:37:02 +0300

    Рефреш при загрузке нового аватара !

--- profile_user/static/profile_user/js/profile.js
+++ profile_user/static/profile_user/js/profile.js
@@ -1,11 +1,35 @@
-let avatars = document.querySelectorAll(".profile__avatar");
-avatars.forEach(avatar => {
-    avatar.style.backgroundImage = "url(" + avatar.dataset.imgurl + ")";
-});
+function setAvatars() {
+    let avatars = document.querySelectorAll(".profile__avatar");
+    avatars.forEach(avatar => {
+        avatar.style.backgroundImage = "url(" + avatar.dataset.imgurl + ")";
+    });
 
-let orderImages = document.querySelectorAll(".order__image");
+    let orderImages = document.querySelectorAll(".order__image");
+    orderImages.forEach(i => {
+        i.style.backgroundImage = "url(" + i.dataset.imgurl + ")";
+    });
+}
+setAvatars();
 
-orderImages.forEach(i => {
-    i.style.backgroundImage = "url(" + i.dataset.imgurl + ")";
+let image_forms = document.querySelectorAll("input[type='file']");
+
+image_forms.forEach(image_form => {
+    image_form.onchange = function (evt) {
+        let tgt = evt.target;
+        let files = tgt.files;
+
+        let image_image = document.querySelector("#" + image_form.dataset.imgid);
+        console.log(image_image)
+        // FileReader
+        if (FileReader && files && files.length) {
+            let fr = new FileReader();
+            fr.onload = function () {
+                image_image.dataset.imgurl = fr.result;
+                image_image.style.zIndex = 200;
+                image_image.style.opacity = 1;
+                setAvatars();
+            }
+            fr.readAsDataURL(files[0]);
+        }
+    }
 });
-    
